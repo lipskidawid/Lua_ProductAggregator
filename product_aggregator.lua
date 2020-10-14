2f326fabff509b9ea0b79d98cb559d0d6cd0f9fe
repl.it user <replituser@example.com>
2020-10-14 07:08:25 +0000

    Change local 'ProductAggregator' name to 'self'

--- product_aggregator.lua
+++ product_aggregator.lua
@@ -3,47 +3,43 @@ local ProductAggregator = {
     sum = 0,
 }
 
-local function addWarrning(message) 
-    ProductAggregator.warrning = ProductAggregator.warrning..message.."\n"
-end
-
-local function validElements(items) 
+local function validElements(self, items) 
     for k, v in pairs(items) do
         -- add handling for negative prices
         if type(v.name) == "string" and type(v.price) == "number" and v.price > 0 then
-            table.insert(ProductAggregator.products, {name = v.name, price = v.price})
+            table.insert(self.products, {name = v.name, price = v.price})
         end
     end
 end
 
-local function sortByPrice()
-    table.sort(ProductAggregator.products, function(a,b) return a.price > b.price end)
+local function sortByPrice(self)
+    table.sort(self.products, function(a,b) return a.price > b.price end)
 end
 
-local function removeDuplicates()
+local function removeDuplicates(self)
     local hash = {}
     local res = {}
 
-    for k, v in ipairs(ProductAggregator.products) do
+    for k, v in ipairs(self.products) do
         if not hash[v.name] then
             res[#res+1] = v
             hash[v.name] = true
-            ProductAggregator.sum = ProductAggregator.sum + v.price
+            self.sum = self.sum + v.price
         end
     end
 
     local tmpProducts = {}
     for i = #res, 1, -1 do table.insert(tmpProducts, res[i]) end
-    ProductAggregator.products = tmpProducts
+    self.products = tmpProducts
 end
 
 function ProductAggregator:new(items)
     -- Valid product name and price
-    validElements(items)
+    validElements(self, items)
     -- Sort element by price
-    sortByPrice()
+    sortByPrice(self)
     -- Remove duplicates
-    removeDuplicates()
+    removeDuplicates(self)
 end
 
 function ProductAggregator:getProduts()
